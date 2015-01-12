2ea2007358cfbad90d285c3a822da6731727501e
Joe Sheehan <joesheehan@nat-oitwireless-inside-vapornet100-a-7420.Princeton.EDU>
2015-01-11 23:45:37 -0500

    part 2 is done. look over it and see what you think.

--- a7/apps/apm.ml
+++ a7/apps/apm.ml
@@ -2,6 +2,8 @@
 open Util
 open Future
 
+(* allows us to switch between ListSeq and our Seq module with ease *)
+(* currently set to ListSeq bc Seq hasn't been implemented *)
 module Mseq = Sequence.ListSeq
 type 'a mySeq =  'a Mseq.t
 
@@ -85,7 +87,7 @@ let print_matches (n : string) ((p, ps) : profile * (float * profile) list) : un
   print_endline ""
 
 
-
+(* turns a filename into a profile list *)
 let load_profiles (filename : string) : profile list =
   let f = open_in filename in
   let rec next accum =
@@ -98,16 +100,32 @@ let load_profiles (filename : string) : profile list =
 
 ;;
 
+module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
+
+
+(*----------------------- GENERIC HELPER FUNCTIONS --------------------------*)
+(* converts a list to a sequence *)
 let list_to_seq (ls : 'a list) :  'a mySeq = 
   Mseq.seq_of_array (Array.of_list ls)
 ;;
 
+(* converts a sequence to a list *)
+let seq_to_list (sequ: 'a mySeq) : 'a list = 
+  Array.to_list (Mseq.array_of_seq sequ)
+;;
+
+(* converts a sequence of type a to a sequence of type a option *)
 let seq_to_option (sequ: 'a mySeq) : 'a option mySeq = 
   let optioner (cur: 'a) : 'a option = Some cur in 
   Mseq.map optioner sequ
 ;;
 
-module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
+(* returns true if two strings are equivalent *)
+let cmpr (a:string)(b:string) : bool = 
+  String.compare a b == 0
+;;
+
+(*-------------------------- THE MATCHING ALGORITHM -------------------------*)
 
 (* apm computes the potential love of your life.  The filename of a file
  * containing candidate profiles is in location 0 of the given array.
@@ -115,23 +133,28 @@ module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
  * of the (human) client are in location 2 and 3, respectively.  The client's
  * profile must be in the profiles file.  Results are output to stdout. *)
 let matchme (args : string array) : unit = 
+
+  (* read inputs in from array *)
   let filename : string = Array.get args 0 in
   let num_matches : int = int_of_string (Array.get args 1) in
+  if num_matches == 0 then raise (Failure "must request > 0 matches");
   let first_name : string = Array.get args 2 in
   let last_name : string = Array.get args 3 in
 
+  (* read profiles from file into sequence*)
   let prof_seq : profile mySeq = list_to_seq (load_profiles filename) in
 
+  (* returns true if p has the same name as the query first and last names *)
+  let name_match (p:profile): bool =
+    if (cmpr p.firstname first_name) && (cmpr p.lastname last_name) 
+    then true else false
+  in 
 
-
-  let prof_finder (fname:string)(lname:string)(ps:profile mySeq) : profile = 
-
-    let name_match (p:profile): bool =
-      if (p.firstname == fname && p.lastname == lname) then true
-      else false
-    in 
-
-    let prof_match (left:profile option) (right:profile option) : profile option  = 
+  (* finds the profile of the query names in the sequence *)
+  let prof_finder (ps:profile mySeq) : profile = 
+    (* returns the valid profile or none from a selection of two *)
+    let prof_match (left:profile option) (right:profile option) 
+      : profile option  = 
       match (left, right) with
           (Some l, None) -> (if name_match l then Some l
                               else None)
@@ -139,61 +162,144 @@ let matchme (args : string array) : unit =
                               else None)
         | (Some lb, Some rb) -> (if name_match rb then Some rb
                                  else if name_match lb then Some lb
-                                 else raise (Failure "our matcher isn't working")) 
-        | (None, None) -> raise (Failure "why are we getting 2 Nones in reduce?!") 
+                                 else raise 
+                                 (Failure "our matcher isn't working")) 
+        | (None, None) -> raise 
+                          (Failure "why are we getting 2 Nones in reduce?!") 
     in
-
     let ps_option = seq_to_option ps in 
-
     match Mseq.reduce prof_match None ps_option with 
         Some a -> a
       | None -> raise (Failure "profile seq doesn't have query profile")
   in 
 
-  let query : profile = prof_finder first_name last_name prof_seq in
+  (* find the query profile *)
+  let query : profile = prof_finder prof_seq in
 
-  let g_o (p:profile) : float = 
-    if query.orientation == p.orientation then (
-      if query.orientation == "straight" then (
-        if query.sex != p.sex then 1.0
-        else 0.0 
+  (* given a profile, computes a float profile tuple where the float is the
+   * compatability value of that profile with the query *)
+  let compat_gen (cur : profile) : (float*profile) = 
+    let grab_bag (p:profile) : float = 
+      (* these are weights for different shared qualities assigned basically 
+       * at random *)
+      let f1 = if cmpr p.profession query.profession then 0.115 else 0.0 in 
+      let f2 = if cmpr p.leisure query.leisure then (f1 +. 0.0863) else f1 in 
+      let f3 = if cmpr p.music query.music then (f2 +. 0.0498) else f2 in 
+      let f4 = if p.drinks == query.drinks then (f3+.0.0472) else f3 in
+      let f5 = if p.smokes == query.smokes then (f4+.0.0367) else f4 in 
+      let f6 = if cmpr p.build query.build then (f5+.0.066) else f5 in 
+      let f7 = if cmpr p.height query.height then (f6+.0.021) else f6 in
+      let f8 = if (p.age >= query.lo_agepref && p.age <= query.hi_agepref) 
+          then (f7+.0.4) else f7 in
+      let f9 = if (query.age >= p.lo_agepref && query.age <= p.hi_agepref) 
+          then (f8+.0.178) else f8 in 
+      f9
+    in 
+
+    (* a multiplier to ensure the query gets back the correct gender and 
+     * orientation candidates *)
+    let gender_orientation (p:profile) : float = 
+      if cmpr query.orientation p.orientation then (
+        if cmpr query.orientation "straight" then (
+          if not (cmpr query.sex p.sex) then 1.0
+          else 0.0
+        )
+        else if cmpr query.sex p.sex then 1.0
+        else 0.0
       )
-      else if query.sex == p.sex then 1.0
       else 0.0
-    )
-    else 0.0
-  in  
+    in  
 
-  let age_q (p:profile) : float = 
-    if (p.age >= query.lo_agepref && p.age <= query.hi_agepref) then 0.4
-    else 0
-  in
+    (* a multiplier to give weight to the query and the candidate's opinions
+     * on children *)
+    let child (p:profile) : float = 
+      if ((p.has_children && query.wants_children) 
+        || (not (p.has_children && query.wants_children))) then 1.0
+      else if ((query.has_children && p.wants_children) 
+                || (not (query.has_children && p.wants_children))) then 0.666
+      else 0.33
+    in  
 
-  let age_p (p:profile) : float = 
-      if (query.age >= p.lo_agepref && query.age <= p.hi_agepref) then 0.178
-    else 0
-  in
+    (* calculates the score, packs as a tuple *)
+    let score = (grab_bag cur)*.(child cur)*.(gender_orientation cur) in
+    (score, cur)
+  in 
 
-  let grab_bag (p:profile) : float = 
-    let f1 = if p.profession == query.profession then 0.115
-             else 0
-    in 
-    let f2 = if p.leisure == query.leisure then (f1 + 0.0863)
-              else f1
-    in 
-    let f3 = if p.music == query.music then (f2 + 0.0498)
-              else f2
-    in 
-    let f4 = if p.drinks == query.drinks then (f3+0.0367)
-              else f3
+  (* computes compatability vectors *)
+  let compat_seq : (float*profile) mySeq = Mseq.map compat_gen prof_seq in 
+
+  (* returns true if to_find is in sequence ps *)
+  let is_in (to_find:float*profile)(ps:(float*profile) mySeq) : bool = 
+    (* compares a tuple of a score and a profile, returning true if they 
+     * are the same *)
+    let equiv (a:float*profile) (b:float*profile) : bool = 
+      match (a, b) with
+      ((sa, pa), (sb, pb)) -> (if (sa == sb && (cmpr pa.firstname pb.firstname) 
+                              && (cmpr pa.lastname pb.lastname))
+                              then true else false) in
+    let equiv_tf = equiv to_find in 
+
+    (* sees if identical profiles are already in the list found so far *)
+    let prof_match (left:(float*profile) option) 
+    (right:(float*profile) option) : (float*profile) option  = 
+      match (left, right) with
+          (Some l, None) -> (if equiv_tf l then Some l else None)
+        | (None, Some r) -> (if equiv_tf r then Some r else None)
+        (* doesn't actually matter that we return the correct prof *)
+        | (Some lb, Some rb) -> Some lb 
+        | (None, None) -> raise (Failure "invalid profile sequence") in
+
+    let ps_option = seq_to_option ps in 
+    match Mseq.reduce prof_match None ps_option with 
+      Some a -> true | None -> false
+  in 
+
+  (* returns the n most compatible profiles from compat_seq that aren't
+   * in sequence soFar *)
+  let rec find_n_best (n:int) (soFar:(float*profile) mySeq) 
+                                          : (float*profile) mySeq = 
+    (* returns true if a's score is greater than b's *)
+    let greater (a:float*profile) (b:float*profile) : bool = 
+      match (a, b) with
+      ((sa, pa), (sb, pb)) -> if sa > sb then true else false
     in
-    let f5 = if p.smokes == query.smokes
 
-  let compatability_gen (cur : profile) : float = 
-    let comp_index
+    (* returns the more compatible of 2 profiles, unless it has 
+     * already been used *)
+    let is_better (left:float*profile) (right:float*profile)
+                                                : (float*profile) = 
+      match (left,right) with
+        ((_, pl),(_, pr)) -> (if name_match pl then right
+                                else if name_match pr then left
+                                else if is_in left soFar then right
+                                else if is_in right soFar then left
+                                else if greater right left then right
+                                else left)
+    in 
 
-  ()
+    let nth_best = Mseq.reduce is_better (0.0, query) compat_seq in 
+    let new_soFar : (float*profile) mySeq = Mseq.cons nth_best soFar in 
+
+    (* recursive call. I think it's ok we aren't using parallelism here
+     * because based on the examples given and general disgression, it seems
+     * as though n will likely be relatively small compared to the size
+     * of the dataset, so there isn't much of a time cost. *)
+    if n == 1 then new_soFar
+    else find_n_best (n-1) new_soFar
+  in 
+
+  (* prints out results *)
+  let matches = find_n_best num_matches (Mseq.empty ()) in 
+  print_matches (string_of_int num_matches) 
+                            (query, (List.rev (seq_to_list matches)))
 ;;
+
+
+
+
+
+
+  
 
 
 
--- a7/apps/inverted_index.ml
+++ a7/apps/inverted_index.ml
@@ -3,67 +3,101 @@ open Util
 
 module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
 
+(* allows us to switch between ListSeq and our Seq module with ease *)
+(* currently set to ListSeq bc Seq hasn't been implemented *)
 module Mseq = Sequence.ListSeq
 type 'a mySeq =  'a Mseq.t 
+
+(* an inverse binding. instead of document to word it is a word matched
+ * with a single document *)
 type inv = {word : string; doc : string}
 
+
+(*----------------------- GENERIC HELPER FUNCTIONS --------------------------*)
+
+(* converts a list to a sequence *)
 let list_to_seq (ls : 'a list) :  'a mySeq = 
-	Mseq.seq_of_array (Array.of_list ls)
+    Mseq.seq_of_array (Array.of_list ls)
 ;;
 
+(* converts a document to a sequence of type inv as defined above *)
 let doc_to_invSeq (doc : document) : inv mySeq = 
-	let title : string = String.lowercase doc.title in
-	let contents : string mySeq =  list_to_seq (Util.split_words doc.contents) in
+    let title : string = string_of_int doc.id in
+    let contents : string mySeq =  
+            list_to_seq (Util.split_words doc.contents) in
 
-	let word_to_inv (cur:string) : inv = 
-		{word = String.lowercase cur; doc = title}
-	in
+    let word_to_inv (cur:string) : inv = 
+        {word = String.lowercase cur; doc = title}
+    in
 
-	Mseq.map word_to_inv contents
+    Mseq.map word_to_inv contents
 ;;
 
+(* returns true if strings a and b are equivalent *)
 let match_word (a:string) (b:string) : bool =
-	if String.compare a b == 0 then true
-	else false 
+    if String.compare a b == 0 then true
+    else false 
 ;;
 
+(* returns a value from list ls that when input into the filter function
+ * produces true *)
 let rec listSearcher (filter:'a -> bool) (ls:'a list) : 'a option = 
-	match ls with 
-		[] -> None
-	  | hd::tl -> (if filter hd then Some hd
-					else listSearcher filter tl)
+    match ls with 
+        [] -> None
+      | hd::tl -> (if filter hd then Some hd
+                    else listSearcher filter tl)
 ;;
 
+
+(*-------------------- the actual reverse index function --------------------*)
 let mkindex (args : string ) : unit = 
-	let docsSeq : document mySeq = list_to_seq (load_documents args) in
-	let invSeqSeq : inv mySeq mySeq = Mseq.map doc_to_invSeq docsSeq in
-	let invSeq : inv mySeq = Mseq.flatten invSeqSeq in 
-	let wordsTable : (string, string list) Hashtbl.t  = Hashtbl.create 1000 in
 
-	let wordListReader (title:string) (word:string) : unit = 
-		let handleFirst : string list = [] in
-		let found : string list =
-			try Hashtbl.find wordsTable word with Not_found -> handleFirst in
-		match listSearcher (match_word title) found with
-			None ->  Hashtbl.replace wordsTable word (title::found);
-		  | Some _ -> ()
-	in
+    (* read in documents from file, and convert to a flat inv sequence *)
+    let docsSeq : document mySeq = list_to_seq (load_documents args) in
+    let invSeqSeq : inv mySeq mySeq = Mseq.map doc_to_invSeq docsSeq in
+    let invSeq : inv mySeq = Mseq.flatten invSeqSeq in 
 
+    (* build a hashtable to store inverse bindings *)
+    let wordsTable : (string, string list) Hashtbl.t  = Hashtbl.create 1000 in
 
-	let invUnpacker (cur:inv) : inv = 
-		wordListReader cur.doc cur.word; cur
-	in
+    (* helper function to be mapped across the inv sequence. takes a word and 
+    title (id as string) and adds it 2 the hashtable if no binding for the word
+    exists yet. if a binding does exist, it replaces it with a binding but with
+    the title id concatenated to the string list of the existing title *)
+    let wordListReader (title:string) (word:string) : unit = 
+        let handleFirst : string list = [] in
+        let found : string list =
+            try Hashtbl.find wordsTable word with Not_found -> handleFirst in
+        match listSearcher (match_word title) found with
+            None ->  Hashtbl.replace wordsTable word (title::found);
+          | Some _ -> ()
+    in
 
-	let toUnit (input:'a) : unit = () in
+    (* feeds an inv into the wordListReader function *)
+    let invUnpacker (cur:inv) : inv = 
+        wordListReader cur.doc cur.word; cur
+    in
 
-	(* generates hash table *)
-	toUnit (Mseq.map invUnpacker invSeq); 
+    (* function that turns the output datatype of another function into unit *)
+    let toUnit (input:'a) : unit = () in
 
-	let hashToList (key:string) (value:string list) (keyval:(string*string list) list) :
-					(string*string list) list = (key,value)::keyval 
-	in
+    (* generates hash table *)
+    toUnit (Mseq.map invUnpacker invSeq); 
 
-	let wordsList = Hashtbl.fold hashToList wordsTable [] in 
+    (* function that takes a key value pair and turns it into a list of 
+     * (key, value) tuples *)
+    let hashToList (key:string) (value:string list) 
+                    (keyval:(string*string list) list) :
+                    (string*string list) list = (key,value)::keyval 
+    in
 
-	print_reduce_results wordsList 
+    (* reads through all bindings in hashtable and converts them into a 
+     * list. I don't think we need parallelism here because it only passes
+     * through the list of words once, which is the same list that has to be
+     * passed through when the results are printed out, so its the same order
+     * of magnitude and not too long. *)
+    let wordsList = Hashtbl.fold hashToList wordsTable [] in 
+
+    (* prints the results *)
+    print_reduce_results wordsList 
 ;;
