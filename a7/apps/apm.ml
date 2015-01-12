91a49f30ae6f126adc64b9ef56784e9fa6eff9fd
Joe Sheehan <joesheehan@nat-oitwireless-inside-vapornet100-a-7420.Princeton.EDU>
2015-01-11 19:52:45 -0500

    "almost done with part 2"

--- a7/apps/apm.ml
+++ a7/apps/apm.ml
@@ -2,6 +2,9 @@
 open Util
 open Future
 
+module Mseq = Sequence.ListSeq
+type 'a mySeq =  'a Mseq.t
+
 type profile = {
   firstname : string;
   lastname : string;
@@ -83,6 +86,27 @@ let print_matches (n : string) ((p, ps) : profile * (float * profile) list) : un
 
 
 
+let load_profiles (filename : string) : profile list =
+  let f = open_in filename in
+  let rec next accum =
+    match (try Some (input_line f) with End_of_file -> None) with
+    | None -> accum
+    | Some line -> next ((convert line)::accum) in
+  let docs = next [] in
+  close_in f;
+  docs
+
+;;
+
+let list_to_seq (ls : 'a list) :  'a mySeq = 
+  Mseq.seq_of_array (Array.of_list ls)
+;;
+
+let seq_to_option (sequ: 'a mySeq) : 'a option mySeq = 
+  let optioner (cur: 'a) : 'a option = Some cur in 
+  Mseq.map optioner sequ
+;;
+
 module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
 
 (* apm computes the potential love of your life.  The filename of a file
@@ -90,7 +114,86 @@ module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
  * The number of matches desired is in location 1.  The first and last name
  * of the (human) client are in location 2 and 3, respectively.  The client's
  * profile must be in the profiles file.  Results are output to stdout. *)
-let matchme (args : string array) : unit = failwith "implement me"
+let matchme (args : string array) : unit = 
+  let filename : string = Array.get args 0 in
+  let num_matches : int = int_of_string (Array.get args 1) in
+  let first_name : string = Array.get args 2 in
+  let last_name : string = Array.get args 3 in
+
+  let prof_seq : profile mySeq = list_to_seq (load_profiles filename) in
+
+
+
+  let prof_finder (fname:string)(lname:string)(ps:profile mySeq) : profile = 
+
+    let name_match (p:profile): bool =
+      if (p.firstname == fname && p.lastname == lname) then true
+      else false
+    in 
+
+    let prof_match (left:profile option) (right:profile option) : profile option  = 
+      match (left, right) with
+          (Some l, None) -> (if name_match l then Some l
+                              else None)
+        | (None, Some r) -> (if name_match r then Some r
+                              else None)
+        | (Some lb, Some rb) -> (if name_match rb then Some rb
+                                 else if name_match lb then Some lb
+                                 else raise (Failure "our matcher isn't working")) 
+        | (None, None) -> raise (Failure "why are we getting 2 Nones in reduce?!") 
+    in
+
+    let ps_option = seq_to_option ps in 
+
+    match Mseq.reduce prof_match None ps_option with 
+        Some a -> a
+      | None -> raise (Failure "profile seq doesn't have query profile")
+  in 
+
+  let query : profile = prof_finder first_name last_name prof_seq in
+
+  let g_o (p:profile) : float = 
+    if query.orientation == p.orientation then (
+      if query.orientation == "straight" then (
+        if query.sex != p.sex then 1.0
+        else 0.0 
+      )
+      else if query.sex == p.sex then 1.0
+      else 0.0
+    )
+    else 0.0
+  in  
+
+  let age_q (p:profile) : float = 
+    if (p.age >= query.lo_agepref && p.age <= query.hi_agepref) then 0.4
+    else 0
+  in
+
+  let age_p (p:profile) : float = 
+      if (query.age >= p.lo_agepref && query.age <= p.hi_agepref) then 0.178
+    else 0
+  in
+
+  let grab_bag (p:profile) : float = 
+    let f1 = if p.profession == query.profession then 0.115
+             else 0
+    in 
+    let f2 = if p.leisure == query.leisure then (f1 + 0.0863)
+              else f1
+    in 
+    let f3 = if p.music == query.music then (f2 + 0.0498)
+              else f2
+    in 
+    let f4 = if p.drinks == query.drinks then (f3+0.0367)
+              else f3
+    in
+    let f5 = if p.smokes == query.smokes
+
+  let compatability_gen (cur : profile) : float = 
+    let comp_index
+
+  ()
+;;
 
 
 
--- a7/apps/inverted_index.ml
+++ a7/apps/inverted_index.ml
@@ -1,10 +1,69 @@
 open Future
+open Util
 
 module PSeq = Sequence.Seq(PFuture)(struct let use_mpi = true end)
 
+module Mseq = Sequence.ListSeq
+type 'a mySeq =  'a Mseq.t 
+type inv = {word : string; doc : string}
 
-(* inverted_index computes an inverted index for the contents of
- * a given file. The filename is the given string.
- * The results are output to stdout. *)
-let mkindex (args : string ) : unit = failwith "implement me"
-	
+let list_to_seq (ls : 'a list) :  'a mySeq = 
+	Mseq.seq_of_array (Array.of_list ls)
+;;
+
+let doc_to_invSeq (doc : document) : inv mySeq = 
+	let title : string = String.lowercase doc.title in
+	let contents : string mySeq =  list_to_seq (Util.split_words doc.contents) in
+
+	let word_to_inv (cur:string) : inv = 
+		{word = String.lowercase cur; doc = title}
+	in
+
+	Mseq.map word_to_inv contents
+;;
+
+let match_word (a:string) (b:string) : bool =
+	if String.compare a b == 0 then true
+	else false 
+;;
+
+let rec listSearcher (filter:'a -> bool) (ls:'a list) : 'a option = 
+	match ls with 
+		[] -> None
+	  | hd::tl -> (if filter hd then Some hd
+					else listSearcher filter tl)
+;;
+
+let mkindex (args : string ) : unit = 
+	let docsSeq : document mySeq = list_to_seq (load_documents args) in
+	let invSeqSeq : inv mySeq mySeq = Mseq.map doc_to_invSeq docsSeq in
+	let invSeq : inv mySeq = Mseq.flatten invSeqSeq in 
+	let wordsTable : (string, string list) Hashtbl.t  = Hashtbl.create 1000 in
+
+	let wordListReader (title:string) (word:string) : unit = 
+		let handleFirst : string list = [] in
+		let found : string list =
+			try Hashtbl.find wordsTable word with Not_found -> handleFirst in
+		match listSearcher (match_word title) found with
+			None ->  Hashtbl.replace wordsTable word (title::found);
+		  | Some _ -> ()
+	in
+
+
+	let invUnpacker (cur:inv) : inv = 
+		wordListReader cur.doc cur.word; cur
+	in
+
+	let toUnit (input:'a) : unit = () in
+
+	(* generates hash table *)
+	toUnit (Mseq.map invUnpacker invSeq); 
+
+	let hashToList (key:string) (value:string list) (keyval:(string*string list) list) :
+					(string*string list) list = (key,value)::keyval 
+	in
+
+	let wordsList = Hashtbl.fold hashToList wordsTable [] in 
+
+	print_reduce_results wordsList 
+;;
